0e230f737bc78605f1a1bc5acb9c7ff03511172b
gluix20 <lx.galvez@gmail.com>
2015-04-23 16:26:19 -0600

    c

--- src/com/example/genz/GenzUI.java
+++ src/com/example/genz/GenzUI.java
@@ -9,11 +9,15 @@ import javax.servlet.annotation.WebServlet;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Resource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.TextField;
@@ -26,9 +30,7 @@ public class GenzUI extends UI implements ClickListener{
 	VerticalLayout layout;
 	TextField kwh;
 	TextField correlativo;
-	TextField a;
-	TextField b;
-	TextField c;
+	TextField tf_nombre;
 	
 	int clicks=0;
 	
@@ -45,22 +47,30 @@ public class GenzUI extends UI implements ClickListener{
 		layout.setMargin(true);
 		
 		setContent(layout);
-
-		kwh = new TextField("kWh");
-		layout.addComponent(kwh);
 		
-		correlativo = new TextField("Correlativo");
+		// Serve the image from the theme
+		Resource res = new ThemeResource("img/genztrans.png");
+		// Display the image without caption
+		Image image = new Image(null, res);
+		image.setWidth("20%");
+		layout.addComponent(image);
+		//layout.setComponentAlignment(image,Alignment.MIDDLE_CENTER);
+		
+
+		kwh = new TextField("kWh:");
+		
+		layout.addComponent(kwh);
+		kwh.setWidth("400px");
+		
+		correlativo = new TextField("Correlativo:");
+		correlativo.setWidth("400px");
 		layout.addComponent(correlativo);
 		
-		//a = new TextField("A:");
-		//layout.addComponent(a);
+		tf_nombre = new TextField("Nombre:");
+		tf_nombre.setWidth("400px");
+		layout.addComponent(tf_nombre);
 		
-		//b = new TextField("B:");
-		//layout.addComponent(b);
-		
-		//c = new TextField("C:");
-		//layout.addComponent(c);
-		
+			
 		Button button = new Button("Calcular");
 		button.addClickListener(this);
 		layout.addComponent(button);
@@ -71,20 +81,25 @@ public class GenzUI extends UI implements ClickListener{
 		clicks++;
 		//layout.addComponent(new Label("Son "+kwh.getValue()+" kWh"));
 		String content = null;
+		String nombre = "";
 		URLConnection connection = null;
 		try {
 		  connection =  new URL("http://www.eegsa.com/saldosyfacturas/factura/dofact.php?el=f&correlativo="+correlativo.getValue()).openConnection();
 		  Scanner scanner = new Scanner(connection.getInputStream());
-		  System.out.println(connection.getInputStream());
+		  
 		  scanner.useDelimiter("\\Z");
 		  content = scanner.next();
 		  scanner.close();
+		  //System.out.println(content.toString());
 		}catch ( Exception ex ) {
 		    ex.printStackTrace();
 		    
 		}
+		nombre = content.substring(content.indexOf("<tr><td class='text-small bold label'>Nombre</td><td class='text-small'>"));
 		content = content.substring(content.indexOf("line1 = "));
+
 		content = content.substring(0,content.indexOf(";"));
+		nombre = nombre.substring("<tr><td class='text-small bold label'>Nombre</td><td class='text-small'>".length(),nombre.indexOf("</td></tr>"));
 		String[] tokens = content.split(",");
 		int[] valores = new int[6];
 		int i = 1;
@@ -107,6 +122,8 @@ public class GenzUI extends UI implements ClickListener{
 		if (clicks>1){
 			layout.removeComponent(layout.getComponent(layout.getComponentCount()-1));			
 		}		
+		//layout.addComponent(new Label("Nombre: "+nombre));
+		tf_nombre.setValue(nombre);
 		layout.addComponent(new Label("Consumo promedio de: "+promedio+" kWh"));
 		
 	}
